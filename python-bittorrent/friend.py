163688f25872abc6367752e86df32dfb34e4e8e2
SonicMisora <sonicmisora@126.com>
2014-06-22 12:12:28 +0800

    add publish

--- python-bittorrent/friend.py
+++ python-bittorrent/friend.py
@@ -21,6 +21,13 @@ class FriendList:
 		res = re.search("'ruid':'(\d+)'", html)
 		if res:
 			self.uid = res.group(1)
+			self.tok = re.search("get_check:'(-?\d+)'", html).group(1)
+			self.rtk = re.search("get_check_x:'([0-9a-zA-Z]*)'", html).group(1)
+
+			#print "uid:", self.uid
+			#print "tok:", self.tok
+			#print "rtk:", self.rtk
+
 			return True
 		else:
 			return False
@@ -49,6 +56,15 @@ class FriendList:
 		print "finished. ", len(self.list), " friends were found."
 		return self.list
 
+	def publish(self, content):
+		page = 'http://shell.renren.com/'+self.uid+'/status'
+		data = {'content': content, 'hostid': self.uid, 'requestToken': self.tok, '_rtk': self.rtk, 'channel': 'renren'}
+		req = urllib2.Request(page, urllib.urlencode(data))
+
+		res = urllib2.urlopen(req).read()
+		return res
+
+
 	def get_my_uid(self):
 		return self.uid
 
@@ -56,4 +72,5 @@ class FriendList:
 if __name__ == "__main__":
 	list = FriendList()
 	list.login("sonicmisora@126.com", "xy")
-	print list.get_list()
+	print list.publish("this is test content")
+	#print list.get_list()
